2649ad8955cacf954273170fbab4db65a6cda3d8
bukadp <thedizain@gmail.com>
2022-04-02 22:52:30 +0300

    player

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import useAuth from '../useAuth';
+import Player from './Player';
 
 function Dashboard({ code }) {
     const accessToken = useAuth(code)
     console.log(accessToken)
     return (
         <div>
-            {code}
+            <Player accessToken={accessToken}/>
         </div>
     )
 }
--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,7 +1,19 @@
-//import React from 'react'
+import React from 'react'
+import SpotifyPlayer from 'react-spotify-web-playback'
 
-function Player () {
+function Player({ accessToken, trackUri }) {
 
+    if (!accessToken) return null
+    return (
+        <div>
+            <SpotifyPlayer
+                token={accessToken}
+                showSaveIcon
+                //uris={trackUri ? [trackUri] : []}
+                uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']}
+            />
+        </div>
+    )
 }
 
 export default Player
