cbd936d6a33af9f806b8c73b0dac08e14ae9c0d3
bukadp <thedizain@gmail.com>
2022-04-23 09:03:36 +0300

    first routs are done

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,5 +1,8 @@
 import { useSelector } from 'react-redux';
 import TrackSearchResult from './TrackSearchResult';
+import {
+  NavLink
+} from "react-router-dom";
 
 function Home(props) {
   const store = useSelector((state) => state.recentlyTracks.recentlyTracks);
@@ -19,7 +22,11 @@ function Home(props) {
           </div>
         </div>
         <div className="home__main-liked">
+        
+        <NavLink to="/likedtracks">
           <div className="home__main-title">Liked Tracks</div>
+          </NavLink>
+
           <div className="home__main-listTracks">
             <div className="home__main-track">Track</div>
           </div>
--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -7,51 +7,75 @@ import useAuth from '../useAuth';
 import Player from './Player';
 import Search from './search';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
 import { useState } from 'react';
 
 function Main({ code }) {
-  const [tab, setTab] = useState('home');
-  const [playList, setPlayList] = useState('');
+  //const [tab, setTab] = useState('home');
+  //const [playList, setPlayList] = useState('');
   const [uri, setUri] = useState('');
   const [togglePlaylist, setTogglePlaylist] = useState(false);
 
   const accessToken = useAuth(code);
 
-  function changeTab(e) {
-    const tab = e.target.id;
-    setTogglePlaylist(false);
-    setPlayList('');
-    setTab(tab);
-  }
+  // function changeTab(e) {
+  //   const tab = e.target.id;
+  //   setTogglePlaylist(false);
+  //   setPlayList('');
+  //   setTab(tab);
+  // }
 
-  function togglePlaysLists() {
-    setPlayList('');
-    setTab('library');
-  }
+  // function togglePlaysLists() {
+  //   setPlayList('');
+  //   setTab('library');
+  // }
 
-  function showPlayList(e) {
-    setPlayList(e.target.id);
-    setTab('');
-  }
+  // function showPlayList(e) {
+  //   setPlayList(e.target.id);
+  //   setTab('');
+  // }
 
   return (
-    <div className="wrapper">
-      {tab === 'home' && <Home />}
-      {tab === 'search' && <Search accessToken={accessToken} setUri={setUri} />}
-      {tab === 'library' && <Library showPlayList={showPlayList} />}
-      <Player accessToken={accessToken} uri={uri} />
-      {playList === 'likedTrack' && (
-        <LikedTracks togglePlaysLists={togglePlaysLists} />
-      )}
-      {playList === 'playList' && (
-        <PlayLists
-          togglePlaysLists={togglePlaysLists}
-          togglePlaylist={togglePlaylist}
-          setTogglePlaylist={setTogglePlaylist}
-        />
-      )}
-      <Navigation changeTab={changeTab} />
-    </div>
+    <Router>
+      <div className="wrapper">
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search accessToken={accessToken} setUri={setUri} />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/myplaylists" element={<PlayLists
+           // togglePlaysLists={togglePlaysLists}
+            togglePlaylist={togglePlaylist}
+            setTogglePlaylist={setTogglePlaylist}
+          />} />
+          <Route path="/likedtracks" element={<LikedTracks />} />
+        </Routes>
+
+        {/* {tab === 'home' && <Home />}
+        {tab === 'search' && <Search accessToken={accessToken} setUri={setUri} />}
+        {tab === 'library' && <Library showPlayList={showPlayList} />}
+        {playList === 'likedTrack' && (
+          <LikedTracks togglePlaysLists={togglePlaysLists} />
+        )}
+        {playList === 'playList' && (
+          <PlayLists
+            togglePlaysLists={togglePlaysLists}
+            togglePlaylist={togglePlaylist}
+            setTogglePlaylist={setTogglePlaylist}
+          />
+        )} */}
+
+
+
+        <Player accessToken={accessToken} uri={uri} />
+        <Navigation />
+      </div>
+    </Router>
   );
 }
 
